b9a2822909fff2311a593760969a50e00fb74a87
hendri2212 <cowok_cool320@yahoo.co.id>
2023-02-22 11:13:51 +0800

    Delete Pengaduan Old

--- pengaduan/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn   = new mysqli('localhost', 'root', '');
-    $db     = mysqli_select_db($conn, 'pengaduan');
-    if ($db) {
-        echo "Koneksi berhasil";
-    } else {
-        echo "Koneksi gagal";
-    }
-?>
