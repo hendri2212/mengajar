5ed8bef3b8360be45ea33e98fe647471cfa7bf36
hendri2212 <cowok_cool320@yahoo.co.id>
2024-08-13 20:20:54 +0800

    Data create file and ui data

---
+++ form/data.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Data Users</title>
+</head>
+<body>
+    <nav class="navbar bg-black text-white mb-2">
+        <div class="container">
+            <!-- <a class="navbar-brand" href="#"> -->
+                <!-- <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> -->
+                <h2>Data Siswa</h2>
+            <!-- </a> -->
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <img src="./assets/thumbnail_image/20240805104305.jpeg" alt="Hendri Arifin" class="img-fluid rounded-circle">
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <tr>
+                                <td class="text-nowrap">Full Name</td>
+                                <td>Hendri Arifin</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>Male</td>
+                            </tr>
+                            <tr>
+                                <td>Religion</td>
+                                <td>Mosleem</td>
+                            </tr>
+                            <tr>
+                                <td>Hoby</td>
+                                <td>Badminton, Volly, Swimming</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <img src="./assets/thumbnail_image/20240805104305.jpeg" alt="Hendri Arifin" class="img-fluid rounded-circle">
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <tr>
+                                <td class="text-nowrap">Full Name</td>
+                                <td>Hendri Arifin</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>Male</td>
+                            </tr>
+                            <tr>
+                                <td>Religion</td>
+                                <td>Mosleem</td>
+                            </tr>
+                            <tr>
+                                <td>Hoby</td>
+                                <td>Badminton, Volly, Swimming</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
